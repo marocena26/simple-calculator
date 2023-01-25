659af940d900c10194f8b7b5558ff6e372be272b
marocena26 <mariaelena.arocena@gmail.com>
2023-01-25 16:00:43 +0100

    add funcionality with JS- create functions

---
+++ js/main.js
@@ -0,0 +1,54 @@
+"use strict";
+
+//CONSTANT
+const numberOne = document.querySelector(".js-number-1");
+const numberTwo = document.querySelector(".js-number-2");
+const result = document.querySelector(".js-result");
+const btn = document.querySelector(".js-btn");
+const select = document.querySelector(".js-select");
+
+//FUNCTIONS
+
+//function to obtain the result according to the operator
+function calculate(numberOneValue, numberTwoValue, selectValue) {
+  let result = "";
+  if (selectValue === "+") {
+    result = numberOneValue + numberTwoValue;
+  } else if (selectValue === "-") {
+    result = numberOneValue - numberTwoValue;
+  } else if (selectValue === "*") {
+    result = numberOneValue * numberTwoValue;
+  } else if (selectValue === "/") {
+    result = numberOneValue / numberTwoValue;
+  } else if (selectValue === "%") {
+    result = numberOneValue % numberTwoValue;
+  }
+
+  return result;
+}
+
+//function to render the result
+function resultText(numberResult) {
+  console.log(result);
+  result.innerHTML = `${numberResult}`;
+}
+
+//event handler function
+function handleClick(ev) {
+  ev.preventDefault();
+
+  if (isNaN(numberOne.value) || isNaN(numberTwo.value)) {
+    console.log("entra");
+    result.innerHTML = "Es necesario introducir dos números válidos";
+  } else {
+    const numberOneValue = parseInt(numberOne.value);
+    const numberTwoValue = parseInt(numberTwo.value);
+    const selectValue = select.value;
+    const numberResult = calculate(numberOneValue, numberTwoValue, selectValue);
+    resultText(numberResult);
+    console.log(numberResult);
+  }
+}
+
+//EVENTS
+btn.addEventListener("click", handleClick);
